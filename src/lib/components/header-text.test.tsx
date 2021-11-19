2d86ec762ef0569ef1851ddcc65a1e84e329c0d7
Thiago Jedi <thiagoa7@gmail.com>
2021-11-19 12:22:30 -0300

    Added tests

---
+++ src/lib/components/header-text.test.tsx
@@ -0,0 +1,10 @@
+﻿import { render } from '@testing-library/preact';
+import HeaderText from './header-text';
+
+describe('<HeaderText/>', () => {
+  it('should show text', () => {
+    const { getByText } = render(<HeaderText text={'test'} />);
+
+    expect(getByText('test')).not.toBe(null);
+  });
+});
